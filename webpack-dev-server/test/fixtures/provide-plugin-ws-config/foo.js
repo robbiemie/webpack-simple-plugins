dc3d5c7ad88b8e5c111fdaffe8cb514f068a619d
robbieyang <robbieyang@tencent.com>
2021-01-07 14:30:19 +0800

    feat: update

--- webpack-dev-server/test/fixtures/provide-plugin-ws-config/foo.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-// 'npm run prepare' must be run for this to work during testing
-const WebsocketClient = require('../../../client/clients/WebsocketClient');
-
-window.expectedClient = WebsocketClient;
-// eslint-disable-next-line camelcase, no-undef
-window.injectedClient = __webpack_dev_server_client__;
